a432c06830481a57c4a60bb1fefc2dbee3ec63ee
Igoy Nawamreh <igoynawamreh@gmail.com>
2020-06-21 10:08:41 +0700

    Change `tab-title` key to `title`.
    and also change the `$tab_title` to `$title`

--- panel.image/engine/r/hook.php
+++ panel.image/engine/r/hook.php
@@ -3,7 +3,7 @@
 function fields($_) {
     extract($GLOBALS, \EXTR_SKIP);
     $has_image_extension = !empty($state->x->image);
-    $tab_title = $state->x->{'panel.image'}->{'tab-title'} ?? 'Image';
+    $title = $state->x->{'panel.image'}->title ?? 'Image';
     $key = $state->x->{'panel.image'}->name ?? 'image';
     $image = (new \Page($_['f']))->{$key};
     $resize_options = (array) ($state->x->{'panel.image'}->rect ?? []);
@@ -22,7 +22,7 @@ if (blob && name) {
 </script>
 JS;
     $_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['image'] = [
-        'title' => $tab_title,
+        'title' => $title,
         'lot' => [
             'fields' => [
                 'type' => 'fields',
