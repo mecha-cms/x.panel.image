ba1643df9fcdbd09e36956ff2b92e9a7e89520b4
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-01-10 20:40:35 +0700

    @mecha-cms/x.panel#19,20

--- panel.image/index/panel.php
+++ panel.image/index/panel.php
@@ -1,5 +1,5 @@
 <?php
 
-if (0 === strpos($_['layout'] . '.', 'page.') && $site->is('page')) {
+if (0 === strpos($_['type'] . '/', 'page/') && $site->is('page')) {
     require __DIR__ . DS . '..' . DS . 'engine' . DS . 'r' . DS . 'hook.php';
 }
