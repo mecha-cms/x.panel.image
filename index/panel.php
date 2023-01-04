592f9bc94708714cc34ad9240d4beeba0d85ff69
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-01-04 12:36:19 +0700

    Disable Trash Button if Field is Vital

--- index/panel.php
+++ index/panel.php
@@ -37,10 +37,10 @@ function _($_) {
                     \D => '/'
                 ]) . '/image.jpg',
                 'icon' => [null, $image && 'set' !== $_['task'] ? [
-                    'd' => $link ? 'M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z' : 'M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z',
-                    'description' => $link ? 'View' : 'Delete',
-                    'link' => $link ? $image : null,
-                    'url' => $link ? null : [
+                    'd' => $link || $vital ? 'M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z' : 'M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z',
+                    'description' => $link || $vital ? 'View' : 'Delete',
+                    'link' => $link || $vital ? $image : null,
+                    'url' => $link || $vital ? null : [
                         'part' => 0,
                         'path' => \substr($image, \strlen('/lot/')),
                         'query' => [
