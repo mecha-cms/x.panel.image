6b2be5b5c8eb80e60b096815aec44497d552fb97
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-02-05 23:04:24 +0700

    Add Image Icon

--- index/panel.php
+++ index/panel.php
@@ -198,6 +198,13 @@ function set($_) {
     return $_;
 }
 
+\Hook::set('_', function ($_) {
+    if (isset($_['lot']['bar']['lot'][0]['lot']['folder']['lot']['image'])) {
+        $_['lot']['bar']['lot'][0]['lot']['folder']['lot']['image']['icon'] = 'M21,17H7V3H21M21,1H7A2,2 0 0,0 5,3V17A2,2 0 0,0 7,19H21A2,2 0 0,0 23,17V3A2,2 0 0,0 21,1M3,5H1V21A2,2 0 0,0 3,23H19V21H3M15.96,10.29L13.21,13.83L11.25,11.47L8.5,15H19.5L15.96,10.29Z';
+    }
+    return $_;
+}, 0);
+
 \Hook::set('_', __NAMESPACE__ . "\\_", 20);
 \Hook::set('do.page.get', __NAMESPACE__ . "\\get", 0);
 \Hook::set('do.page.set', __NAMESPACE__ . "\\set", 0);
