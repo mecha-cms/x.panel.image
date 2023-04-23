7feadf95ba317553c083ec5401bb1b07e26c47cf
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-04-23 23:40:46 +0700

    Update

--- index/panel.php
+++ index/panel.php
@@ -10,6 +10,10 @@ if (!empty($state->x->{'panel.image'}->description)) {
     ];
 }
 
+if (!\array_key_exists('type', $_GET) && !isset($_['type']) && 'set' === $_['task'] && 0 === \strpos($_['path'] . '/', 'image/')) {
+    $GLOBALS['_']['type'] = $_['type'] = 'blob/image';
+}
+
 function _($_) {
     if (0 === \strpos($_['type'] . '/', 'blob/image/')) {
         if (isset($_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['blob']['lot']['fields']['lot']['blob'])) {
@@ -392,7 +396,8 @@ function set($_) {
     }
     if (0 === \strpos($_['path'] . '/', 'image/')) {
         $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['blob']['icon'] = 'M13 19C13 19.7 13.13 20.37 13.35 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H19C20.11 3 21 3.9 21 5V13.35C20.37 13.13 19.7 13 19 13V5H5V19H13M13.96 12.29L11.21 15.83L9.25 13.47L6.5 17H13.35C13.75 15.88 14.47 14.91 15.4 14.21L13.96 12.29M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z';
-        $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['blob']['url']['query']['type'] = 'blob/image';
+        $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['blob']['title'] = 'Image';
+        $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['blob']['url']['query']['type'] = null;
         $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['file']['skip'] = true; // Disable file button
         if (isset($_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['files'])) {
             $_['lot']['desk']['lot']['form']['lot'][0]['title'] = 'Image';
